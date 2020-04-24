8c378f41e14b0c2b574c5d08cf47952b261dd0fc
juan <juancampos@upeu.edu.pe>
2020-04-24 13:43:25 -0500

    Calculadora

---
+++ Calculadora/Calculadora.java
@@ -0,0 +1,26 @@
+package Calculadora;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        
+        double n1 = 10;
+        double n2 = 5;
+        
+        //suma
+        Suma sum = new Suma(n1,n2);
+        sum.mostrarResultado();
+        
+        //resta
+        Resta res = new Resta(n1,n2);
+        res.mostrarResultado();
+        
+        //multiplicacion
+        Multiplicacion mul = new Multiplicacion(n1,n2);
+        mul.mostrarResultado();
+        
+        //division
+        Division div = new Division(n1,n2);
+        div.mostrarResultado();
+    }
+}
